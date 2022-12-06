f8f7841ac7614780d4ce41296c2fbf3f052b6fb2
Lambertus <bnieuwhof@gmail.com>
2022-12-06 16:02:09 +0000

    1st commit

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#####################
+# Setting up Server #
+#####################
+
+apt install -y ansible
+git clone git@github.com:Nieuwhof/ansible.git
+git clone git@github.com:Nieuwhof/multipass.git
+cd ansible
+ansible-playbook -k -i inventory/hosts setup.yaml
+su ops
+cd /home/ops/cloud
+sudo ./build.sh gromit
+sudo ./build.sh preston
