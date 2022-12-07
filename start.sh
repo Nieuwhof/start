cfcd14c355494e18d971f8f79f74674e9a1fc64c
Nieuwhof <56723391+Nieuwhof@users.noreply.github.com>
2022-12-07 13:55:34 +0100

    sd

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Nieuwhof/ansible.git &&
 git clone https://github.com/Nieuwhof/multipass.git &&
 cd ~/ansible && 
 ansible-playbook -k -i inventory/hosts setup.yaml &&
+echo "Please do a ctrl C to continue"
 cd /home/ops/cloud &&
 su ops &&
 sudo ./build.sh gromit && 
