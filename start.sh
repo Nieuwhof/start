3708147932ec11e65c3bab785888f68d686cf29d
Nieuwhof <56723391+Nieuwhof@users.noreply.github.com>
2022-12-07 13:57:52 +0100

    ss

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ git clone https://github.com/Nieuwhof/ansible.git &&
 git clone https://github.com/Nieuwhof/multipass.git &&
 cd ~/ansible && 
 ansible-playbook -k -i inventory/hosts setup.yaml &&
-echo "Please do a ctrl C to continue"
+echo "Please type EXIT at the prmpt to continue"
 cd /home/ops/cloud &&
 su ops &&
 sudo ./build.sh gromit && 
