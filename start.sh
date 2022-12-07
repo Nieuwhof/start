c5d1e0e5d0f14f267ac5be223384d40e5adf2598
Nieuwhof <56723391+Nieuwhof@users.noreply.github.com>
2022-12-07 10:56:59 +0100

    added &&

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 # Setting up Server #
 #####################
 
-apt install -y ansible
-git clone git@github.com:Nieuwhof/ansible.git
-git clone git@github.com:Nieuwhof/multipass.git
-cd ansible
-ansible-playbook -k -i inventory/hosts setup.yaml
-su ops
-cd /home/ops/cloud
-sudo ./build.sh gromit
+apt install -y ansible &&
+git clone git@github.com:Nieuwhof/ansible.git && 
+git clone git@github.com:Nieuwhof/multipass.git &&
+cd ~/ansible && 
+ansible-playbook -k -i inventory/hosts setup.yaml &&
+su ops &&
+cd /home/ops/cloud &&
+sudo ./build.sh gromit && 
 sudo ./build.sh preston
