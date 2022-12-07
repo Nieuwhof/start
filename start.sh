a5aa2461f05d75535056e14776d00fcded1f902c
Nieuwhof <56723391+Nieuwhof@users.noreply.github.com>
2022-12-07 12:43:45 +0100

    5minute snooze

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ git clone https://github.com/Nieuwhof/ansible.git &&
 git clone https://github.com/Nieuwhof/multipass.git &&
 cd ~/ansible && 
 ansible-playbook -k -i inventory/hosts setup.yaml &&
-sleep 3m &&
+sleep 5m &&
 su ops &&
 cd /home/ops/cloud &&
 sudo ./build.sh gromit && 
