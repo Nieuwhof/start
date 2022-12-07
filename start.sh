b29088ac1c162450f4019ce3bb5ec738ed9f3498
Nieuwhof <56723391+Nieuwhof@users.noreply.github.com>
2022-12-07 13:39:12 +0100

    5

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ git clone https://github.com/Nieuwhof/ansible.git &&
 git clone https://github.com/Nieuwhof/multipass.git &&
 cd ~/ansible && 
 ansible-playbook -k -i inventory/hosts setup.yaml &&
-sleep 5m &&
+sleep 5 &&
 su ops &&
 cd /home/ops/cloud &&
 sudo ./build.sh gromit && 
