857e1d923865022e531dec260f430ddc62203abb
Stuiver <56723391+Nieuwhof@users.noreply.github.com>
2023-02-24 06:49:52 -0600

    Update VpnSetup.sh

--- VpnSetup.sh
+++ VpnSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install ca-certificates curl gnupg -y
-curl -L https://pkgs.wiretrustee.com/debian/public.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/wiretrusstee.gpg
+curl -L https://pkgs.wiretrustee.com/debian/public.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/wiretrustee.gpg
 echo 'deb https://pkgs.wiretrustee.com/debian stable main' | sudo tee /etc/apt/sources.list.d/wiretrustee.list
 sudo apt-get
 sudo apt-get install netbird
